a5f97b96e56db3bd682e9e2bfdeb8f6511d90948
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-05-30 18:32:20 -0300

    feat: added example entities

---
+++ entities/category.ts
@@ -0,0 +1,10 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm/browser';
+
+@Entity('category')
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+}
